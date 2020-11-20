756d97445779ed181674a696db4a8f657365d8e7
Marvin Kome <marvinkome@gmail.com>
2020-11-20 12:40:50 +0100

    remove website slider

--- website/src/LandingPage/index.js
+++ website/src/LandingPage/index.js
@@ -1,29 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Screenshot from "../assets/screenshot.png";
 import playstore from "../assets/playstore.svg";
 import { Header } from "./Header";
 
 import "./LandingPage.scss";
 
-const headerText = [
-  "Pay anyone with a phone number",
-  "Withdraw money with app to your bank",
-  "Free app to app payments. ₦5 SMS fee non-app users",
-];
-
 function App() {
-  const [activeSlide, setActiveSlide] = useState(0);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setActiveSlide(
-        activeSlide === headerText.length - 1 ? 0 : activeSlide + 1
-      );
-    }, 1500);
-
-    return () => clearTimeout(timeout);
-  }, [activeSlide]);
-
   return (
     <div className="App">
       <Header />
@@ -32,7 +14,7 @@ function App() {
         <div className="grid-1">
           <div className="grid-content">
             <h1 className="text fade">
-              <span>{headerText[activeSlide]}</span>
+              <span>Pay anyone with a phone number in seconds</span>
             </h1>
 
             <div className="button-container">
@@ -42,23 +24,6 @@ function App() {
                   <img alt="playstore" src={playstore}></img>
                 </span>
               </button>
-
-              <div className="dot-container">
-                {headerText.map((_, idx) => {
-                  let className = "dot";
-                  if (activeSlide === idx) {
-                    className += " active";
-                  }
-
-                  return (
-                    <span
-                      onClick={() => setActiveSlide(idx)}
-                      key={idx}
-                      className={className}
-                    ></span>
-                  );
-                })}
-              </div>
             </div>
           </div>
         </div>

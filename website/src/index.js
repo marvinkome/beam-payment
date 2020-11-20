d2e988f81f5b72542c869256da3f638addd1fad2
Marvin Kome <marvinkome@gmail.com>
2020-11-20 15:03:45 +0100

    add google analytics

--- website/src/index.js
+++ website/src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
 import LandingPage from "./LandingPage";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+
+ReactGA.initialize("UA-183688411-1", {
+  debug: process.env.NODE_ENV === "development",
+});
 
 ReactDOM.render(
   <React.StrictMode>
